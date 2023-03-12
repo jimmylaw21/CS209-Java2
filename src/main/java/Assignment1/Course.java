36b6c4d823f9edee84aec13acea8b69473c1c0b4
jimmylaw21 <1908486736@qq.com>
2023-03-12 16:17:38 +0800

    commit

---
+++ src/main/java/Assignment1/Course.java
@@ -0,0 +1,163 @@
+package Assignment1;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Course {
+    private String institution;
+    private String courseNumber;
+    private LocalDate launchDate;
+    private String courseTitle;
+    private List<String> instructors;
+    private String courseSubject;
+    private int year;
+    private boolean hasHonorCodeCert;
+    private int participantsCount;
+    private int auditedCount;
+    private int certifiedCount;
+    private double auditedPercent;
+    private double certifiedPercent;
+    private double certifiedPercent50;
+    private double videoPlayedPercent;
+    private double forumPostedPercent;
+    private double gradeHigherThanZeroPercent;
+    private double totalCourseHours;
+    private double medianHoursCertification;
+    private double medianAge;
+    private double malePercent;
+    private double femalePercent;
+    private double bachelorsOrHigherPercent;
+
+    // constructor
+    public Course(String institution, String courseNumber, LocalDate launchDate, String courseTitle,
+                  List<String> instructors, String courseSubject, int year, boolean hasHonorCodeCert,
+                  int participantsCount, int auditedCount, int certifiedCount, double auditedPercent,
+                  double certifiedPercent, double certifiedPercent50, double videoPlayedPercent,
+                  double forumPostedPercent, double gradeHigherThanZeroPercent, double totalCourseHours,
+                  double medianHoursCertification, double medianAge, double malePercent, double femalePercent,
+                  double bachelorsOrHigherPercent) {
+        this.institution = institution;
+        this.courseNumber = courseNumber;
+        this.launchDate = launchDate;
+        this.courseTitle = courseTitle;
+        this.instructors = instructors;
+        this.courseSubject = courseSubject;
+        this.year = year;
+        this.hasHonorCodeCert = hasHonorCodeCert;
+        this.participantsCount = participantsCount;
+        this.auditedCount = auditedCount;
+        this.certifiedCount = certifiedCount;
+        this.auditedPercent = auditedPercent;
+        this.certifiedPercent = certifiedPercent;
+        this.certifiedPercent50 = certifiedPercent50;
+        this.videoPlayedPercent = videoPlayedPercent;
+        this.forumPostedPercent = forumPostedPercent;
+        this.gradeHigherThanZeroPercent = gradeHigherThanZeroPercent;
+        this.totalCourseHours = totalCourseHours;
+        this.medianHoursCertification = medianHoursCertification;
+        this.medianAge = medianAge;
+        this.malePercent = malePercent;
+        this.femalePercent = femalePercent;
+        this.bachelorsOrHigherPercent = bachelorsOrHigherPercent;
+    }
+
+    // getters
+    public String getInstitution() {
+        return institution;
+    }
+
+    public String getCourseNumber() {
+        return courseNumber;
+    }
+
+    public LocalDate getLaunchDate() {
+        return launchDate;
+    }
+
+    public String getCourseTitle() {
+        return courseTitle;
+    }
+
+    public List<String> getInstructors() {
+        return instructors;
+    }
+
+    public String getCourseSubject() {
+        return courseSubject;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public boolean hasHonorCodeCert() {
+        return hasHonorCodeCert;
+    }
+
+    public int getParticipantsCount() {
+        return participantsCount;
+    }
+
+    public int getAuditedCount() {
+        return auditedCount;
+    }
+
+    public int getCertifiedCount() {
+        return certifiedCount;
+    }
+
+    public double getAuditedPercent() {
+        return auditedPercent;
+    }
+
+    public double getCertifiedPercent() {
+        return certifiedPercent;
+    }
+
+    public double getCertifiedPercent50() {
+        return certifiedPercent50;
+    }
+
+    public boolean isHasHonorCodeCert() {
+        return hasHonorCodeCert;
+    }
+
+    public double getVideoPlayedPercent() {
+        return videoPlayedPercent;
+    }
+
+    public double getForumPostedPercent() {
+        return forumPostedPercent;
+    }
+
+    public double getGradeHigherThanZeroPercent() {
+        return gradeHigherThanZeroPercent;
+    }
+
+    public double getTotalCourseHours() {
+        return totalCourseHours;
+    }
+
+    public double getMedianHoursCertification() {
+        return medianHoursCertification;
+    }
+
+    public double getMedianAge() {
+        return medianAge;
+    }
+
+    public double getMalePercent() {
+        return malePercent;
+    }
+
+    public double getFemalePercent() {
+        return femalePercent;
+    }
+
+    public double getBachelorsOrHigherPercent() {
+        return bachelorsOrHigherPercent;
+    }
+}
+
+
+
--- src/main/java/Assignment1/OnlineCoursesAnalyzer.java
+++ src/main/java/Assignment1/OnlineCoursesAnalyzer.java
@@ -1,4 +1,289 @@
 package Assignment1;
 
+import java.io.*;
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
+
+//use techniques such as Collections, Lambda, and Streams, which we've covered in the lectures.
 public class OnlineCoursesAnalyzer {
+    private final List<Course> courses;
+    private final Set<String> instuctors;
+    public OnlineCoursesAnalyzer(String datasetPath) {
+        courses = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(datasetPath))) {
+            // skip header
+            br.readLine();
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = csvSplit(line);
+                // create Course object from CSV row
+                Course course = new Course(
+                        values[0],
+                        values[1],
+                        parseDate(values[2]),
+                        values[3],
+                        Arrays.asList(values[4].split(",")),
+                        values[5],
+                        Integer.parseInt(values[6]),
+                        Boolean.parseBoolean(values[7]),
+                        Integer.parseInt(values[8]),
+                        Integer.parseInt(values[9]),
+                        Integer.parseInt(values[10]),
+                        Double.parseDouble(values[11]),
+                        Double.parseDouble(values[12]),
+                        Double.parseDouble(values[13]),
+                        Double.parseDouble(values[14]),
+                        Double.parseDouble(values[15]),
+                        Double.parseDouble(values[16]),
+                        Double.parseDouble(values[17]),
+                        Double.parseDouble(values[18]),
+                        Double.parseDouble(values[19]),
+                        Double.parseDouble(values[20]),
+                        Double.parseDouble(values[21]),
+                        Double.parseDouble(values[22])
+                );
+                courses.add(course);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        instuctors = courses.stream().flatMap(course -> course.getInstructors().stream()).collect(Collectors.toSet());
+    }
+
+    public static LocalDate parseDate(String date) {
+        String[] values = date.split("/");
+        return LocalDate.of(Integer.parseInt(values[2]), Integer.parseInt(values[0]), Integer.parseInt(values[1]));
+    }
+
+    public static String[] csvSplit(String str) {
+        List<String> list = new ArrayList<>();
+        int start = 0;
+        int end = 0;
+        boolean flag = false;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '"') {
+                flag = !flag;
+            }
+            if (str.charAt(i) == ',' && !flag) {
+                end = i;
+                list.add(str.substring(start, end));
+                start = i + 1;
+            }
+        }
+        list.add(str.substring(start));
+        return list.toArray(new String[0]);
+    }
+
+    //    This method returns a <institution, count> map, where the key is the institution while the value is the
+    //    total number of participants who have accessed the courses of the institution.
+    //    The map should be sorted by the alphabetical order of the institution.
+    public Map<String, Integer> getPtcpCountByInst(){
+        return courses.stream()
+                .collect(Collectors.groupingBy(Course::getInstitution, Collectors.summingInt(Course::getParticipantsCount)))
+                .entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (a, b) -> a, LinkedHashMap::new));
+    }
+    //This method returns a <institution-course Subject, count> map, where the key is the string
+    //concatenating the Institution and the course Subject (without quotation marks) using '-' while the value is
+    //the total number of participants in a course Subject of an institution.
+    //The map should be sorted by descending order of count (i.e., from most to least participants). If two
+    //participants have the same count, then they should be sorted by the alphabetical order of the
+    //institution-course Subject.
+    public Map<String, Integer> getPtcpCountByInstAndSubject(){
+        return courses.stream()
+                .collect(Collectors.groupingBy(course -> course.getInstitution() + "-" + course.getCourseSubject(), Collectors.summingInt(Course::getParticipantsCount)))
+                .entrySet().stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed().thenComparing(Map.Entry.comparingByKey()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (a, b) -> a, LinkedHashMap::new));
+    }
+//    An instructor may be responsible for multiple courses, including independently responsible courses and codeveloped courses.
+//    This method returns a <Instructor, [[course1, course2,...],[coursek,coursek+1,...]]>
+//    map, where the key is the name of the instructor (without quotation marks) while the value is a list
+//    containing 2-course lists, where List 0 is the instructor's independently responsible courses, if s/he has no
+//    independently responsible courses, this list also needs to be created, but with no elements. List 1 is the
+//    instructor's co-developed courses, if there are no co-developed courses, do the same as List 0. Note that
+//    the course title (without quotation marks) should be sorted by alphabetical order in the list, and the case of
+//    identical names should be treated as the same person.
+    public Map<String, List<List<String>>> getCourseListOfInstructor(){
+        Map<String, List<List<String>>> map = new HashMap<>();
+        for (String instuctor : instuctors) {
+            List<List<String>> list = new ArrayList<>();
+            List<String> list1 = new ArrayList<>();
+            List<String> list2 = new ArrayList<>();
+            for (Course course : courses) {
+                if (course.getInstructors().contains(instuctor)) {
+                    if (course.getInstructors().size() == 1) {
+                        list1.add(course.getCourseTitle());
+                    } else {
+                        list2.add(course.getCourseTitle());
+                    }
+                }
+            }
+            list1.sort(String::compareTo);
+            list2.sort(String::compareTo);
+            list.add(list1);
+            list.add(list2);
+            map.put(instuctor, list);
+        }
+        return map;
+    }
+//    This method returns the top K courses (parameter topK) by the given criterion (parameter by). Specifically,
+//    by="hours": the results should be courses sorted by descending order of Total Course Hours
+//            (Thousands) (from the longest course to the shortest course).
+//    by="participants": the results should be courses sorted by descending order of the number of
+//    the Participants (Course Content Accessed) (from the most to the least).
+//    Note that the results should be a list of Course titles. If two courses have the same total Course hours or
+//    participants, then they should be sorted by alphabetical order of their titles. The same course title can only
+//    occur once in the list.
+    public List<String> getCourses(int topK, String by){
+        return courses.stream()
+                .sorted((a, b) -> {
+                    if (by.equals("hours")) {
+                        if (a.getTotalCourseHours() == b.getTotalCourseHours()) {
+                            return a.getCourseTitle().compareTo(b.getCourseTitle());
+                        } else {
+                            return Double.compare(b.getTotalCourseHours(), a.getTotalCourseHours());
+                        }
+                    } else {
+                        if (a.getParticipantsCount() == b.getParticipantsCount()) {
+                            return a.getCourseTitle().compareTo(b.getCourseTitle());
+                        } else {
+                            return Integer.compare(b.getParticipantsCount(), a.getParticipantsCount());
+                        }
+                    }
+                })
+                .map(Course::getCourseTitle)
+                .distinct()
+                .limit(topK)
+                .collect(Collectors.toList());
+    }
+//    This method searches courses based on three criteria:
+//    courseSubject: Fuzzy matching is supported and case insensitive. If the inputcourseSubject is
+//    "science", all courses whose course subject includes "science" or "Science" or whatever (case insensitive) meet the criteria.
+//    percentAudited: the percent of the audited should >= percentAudited
+//    totalCourseHours: the Total Course Hours (Thousands) should <= totalCourseHours
+//    Note that the results should be a list of course titles that meet the given criteria, and sorted by alphabetical
+//    order of the titles. The same course title can only occur once in the list.
+    public List<String> searchCourses(String courseSubject, double
+            percentAudited, double totalCourseHours){
+        return courses.stream()
+                .filter(course -> course.getCourseSubject().toLowerCase().contains(courseSubject.toLowerCase()))
+                .filter(course -> course.getAuditedPercent() >= percentAudited)
+                .filter(course -> course.getTotalCourseHours() <= totalCourseHours)
+                .map(Course::getCourseTitle)
+                .distinct()
+                .sorted(String::compareTo)
+                .collect(Collectors.toList());
+    }
+//    This method recommends 10 courses based on the following input parameter:
+//    age: age of the user
+//    gender: 0-female, 1-male
+//    isBachelorOrHigher: 0-Not get bachelor degree, 1- Bachelor degree or higher
+//    First, calculate the average Median Age, average % Male, and average % Bachelor's Degree or
+//    Higher for each course. Note that Course Number is the unique id of each course;
+//    Secondly, the following formula:
+//    $similarity value= (age -average Median Age)^2 + (gender100 - average Male)^2 + (isBachelorOrHigher100
+//- average Bachelor's Degree or Higher)^2$
+//    is used to calculate the similarity between the characteristics of the input user and the characteristics of
+//    each course's participants. The higher the similarity, the smaller the value;
+//    Finally, return the top 10 courses with the smallest similarity value.
+//    Note that the results should be a list of course titles. A Course Number may correspond to different
+//    course titles, please return the course title with the latest Launch Date and the same course
+//    title can only occur once in the list. The courses should be sorted by their similarity values. If two courses
+//    have the same similarity values, then they should be sorted by alphabetical order of their titles.
+    public List<String> recommendCourses(int age, int gender, int
+            isBachelorOrHigher){
+        List<CourseStats> statsList = new ArrayList<>();
+        for (Course course : courses) {
+            CourseStats stats = new CourseStats(course);
+            statsList.add(stats);
+        }
+
+        double avgAge = statsList.stream().mapToDouble(CourseStats::getMedianAge).average().orElse(0);
+        double avgMale = statsList.stream().mapToDouble(CourseStats::getMalePercent).average().orElse(0);
+        double avgBachelorsOrHigher = statsList.stream().mapToDouble(CourseStats::getBachelorsOrHigherPercent).average().orElse(0);
+
+        List<CourseStats> recommendedStats = new ArrayList<>();
+        for (CourseStats stats : statsList) {
+            double similarity = Math.pow(age - stats.getMedianAge(), 2)
+                    + Math.pow(gender * 100 - stats.getMalePercent(), 2)
+                    + Math.pow(isBachelorOrHigher * 100 - stats.getBachelorsOrHigherPercent(), 2);
+            stats.setSimilarity(similarity);
+            recommendedStats.add(stats);
+        }
+
+        Collections.sort(recommendedStats);
+
+        List<String> recommendedCourses = new ArrayList<>();
+        for (int i = 0; i < Math.min(recommendedStats.size(), 10); i++) {
+            recommendedCourses.add(recommendedStats.get(i).getCourseTitle());
+        }
+
+        return recommendedCourses;
+    }
+
+    private class CourseStats implements Comparable<CourseStats> {
+        private Course course;
+        private double medianAge;
+        private double malePercent;
+        private double bachelorsOrHigherPercent;
+        private double similarity;
+
+        public CourseStats(Course course) {
+            this.course = course;
+            this.medianAge = course.getMedianAge();
+            this.malePercent = course.getMalePercent();
+            this.bachelorsOrHigherPercent = course.getBachelorsOrHigherPercent();
+        }
+
+        public String getCourseTitle() {
+            return course.getCourseTitle();
+        }
+
+        public double getMedianAge() {
+            return medianAge;
+        }
+
+        public double getMalePercent() {
+            return malePercent;
+        }
+
+        public double getBachelorsOrHigherPercent() {
+            return bachelorsOrHigherPercent;
+        }
+
+        public void setSimilarity(double similarity) {
+            this.similarity = similarity;
+        }
+
+        @Override
+        public int compareTo(CourseStats other) {
+            return Double.compare(similarity, other.similarity);
+        }
+    }
+
+    public static void main(String[] args) {
+//        OnlineCoursesAnalyzer analyzer = new OnlineCoursesAnalyzer("C:\\Users\\jimmylaw21\\OneDrive - 南方科技大学\\桌面\\CS209 java2\\src\\main\\java\\Assignment1\\local.csv");
+        OnlineCoursesAnalyzer analyzer = new OnlineCoursesAnalyzer("local.csv");
+//        System.out.println(analyzer.getPtcpCountByInst());
+//        System.out.println(analyzer.getPtcpCountByInstAndSubject());
+//        System.out.println(analyzer.getCourseListOfInstructor());
+//        System.out.println(analyzer.getCourses(10, "hours"));
+//        System.out.println(analyzer.searchCourses("science", 0.1, 1000));
+        System.out.println(analyzer.recommendCourses(30, 1, 1));
+
+    }
+
+    public static String mapToString(Map<String, Integer> map) {
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            sb.append(entry.getKey()).append(" : ").append(entry.getValue()).append("\n");
+        }
+        return sb.toString();
+    }
+
 }
